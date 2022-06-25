00c1242804df914242c868cdac21975d93420a05
ram kumar <rammaniyari@gmail.com>
2022-06-25 08:38:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+
+// console.log("******************************************************")
+// console.log("node js ")
+// console.log("******************************************************")
+
+
+// console.log("__________________________________________________")
+
+
+// console.log("Hello node jss ")  
+// console.log(global)
+// console.log(__dirname)
+// console.log(__filename)
+
+
+// function addNuber(){
+
+// }
+
+
+// const os = require('os');
+// console.log(os.version())
+// console.log(os.platform())
+// console.log(os.cpus())
+// console.log(os.hostname())
+
+// console.log("__________________________________________________")
+
+// function addNumber(a,b){
+//     return a + b;
+// }
+// module.exports = addNumber;
+
+// const addNuber = require('./operations')
+// const result = addNuber(2,4);
+// console.log(result)  // 6
+// console.log("__________________________________________________")
+
+
+// function addNumber(a,b){
+//     return a + b;
+// }
+// function subNumber(a,b){
+//     return a - b
+// }
+// module.exports = {
+//     addNumber,
+//     subNumber
+// }
+
+// const {addNumber,subNumber}= require('./operations')
+// const sum  = addNumber(3,4);
+// console.log(sum)
+// const sub = subNumber(4,5);
+// console.log(sub);
+
+// console.log("__________________________________________________")
+
+// exports.addNumber = (a,b) =>{
+//     return a + b;
+// }
+// exports.subNumber =(a,b) =>{
+//     return a - b
+// }
+
+// // localModule
+// // operaton js file 
+// const {addNumber,subNumber}= require('./operations')
+// const sum  = addNumber(3,4);
+// console.log(sum)
+// const sub = subNumber(4,5);
+// console.log(sub);
+
+// console.log("__________________________________________________")
+
+// const axios = require('axios');
+// console.log(axios)
+
+// console.log("__________________________________________________")
